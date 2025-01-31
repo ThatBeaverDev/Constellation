21a4238ce8e9c91437558817b00453566addb0b7
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-01-31 16:52:40 +0000

    Update rotur.js

--- bin/rotur.js
+++ bin/rotur.js
@@ -5,6 +5,15 @@ async function init(args) {
     let resp
     args.splice(0,1)
     switch(cmd) {
+        //case "install":
+        //    resp = await system.fetchURL("https://apps.mistium.com/install?appname=" + args[0] + "&language=crl")
+        //    system.log(resp)
+        //    break;
+        case "run":
+            resp = await system.fetchURL("https://apps.mistium.com/install?appname=" + args[0] + "&language=crl")
+            system.files.writeFile("/temp.crl",resp)
+            system.startProcess("/temp.crl")
+            break;
         case "login":
             system.rotur.usr = args[0]
             system.rotur.pswd = args[1]
