13812bc534d962f4a2c7ffa512010f849d750a9e
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-02-15 20:22:50 +0000

    Add kill and killall commands

---
+++ bin/killall.js
@@ -0,0 +1,21 @@
+// kill all processes with a set directory
+
+function init(args) {
+    if (args[0] == undefined) {
+        system.post("usage: killall [processDirectory]\nYou must specify the process directory to kill.")
+    }
+
+    let toKill = []
+
+    // math the processes
+    for (const i in system.processes) {
+        if (system.processes[i].name == args[0]) {
+            toKill.push(i)
+        }
+    }
+
+    // kill the processes
+    for (const i in toKill) {
+        system.stopProcess(i)
+    }
+}
