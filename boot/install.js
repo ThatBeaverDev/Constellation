456e3283810e306815db6166f2a5697bd98eaa8f
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-02-25 12:27:22 +0000

    Create install script

---
+++ boot/install.js
@@ -0,0 +1,17 @@
+async function install() {
+    const Name = "/boot/install.js"
+
+    system.post(Name,"Installing Packages...")
+    let i = await system.fetchURL(system.baseURI + "/index.json")
+    const index = JSON.parse(i).packages
+
+    for (const i in index) {
+        system.eval("aurora install " + index[i], "", true)
+    }
+
+    system.installed = true
+
+    system.post(Name, "Installation Complete")
+}
+
+install()
