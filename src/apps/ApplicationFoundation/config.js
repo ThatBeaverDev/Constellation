a4a578b147b739319c837eabfe3013bb0341ef2e
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-06-21 20:51:42 +0100

    Add ApplicationFoundation

---
+++ src/apps/ApplicationFoundation/config.js
@@ -0,0 +1,7 @@
+export default {
+	name: "ApplicationFoundation Demo App",
+	description: "An Example app from which all others are forked..",
+	category: "Productivity",
+	author: "Constellation",
+	version: 1
+};
