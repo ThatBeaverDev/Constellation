af8b6696a7c9664d45936ac96d5f584717e0e2bd
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-06-22 14:44:39 +0100

    Move browserFS to an external folder

--- eslint.config.js
+++ eslint.config.js
@@ -33,7 +33,6 @@ export default defineConfig([
 
   // global ignores should be last or separate
   globalIgnores([
-    "./src/lib/browserfs.js",
-    "./src/lib/browserfs.min.js"
+    "./src/lib/external"
   ]),
 ]);
