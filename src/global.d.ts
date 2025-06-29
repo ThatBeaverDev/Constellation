0b1c4193c25c191347902abf8b353f7c4d3b7da4
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-06-29 20:56:51 +0100

    Update global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,17 @@
 // globals.d.ts
 import * as envType from "./apps/api.js";
+import * as executables from "./apps/executables.js";
 
 export {}; // mark as module to allow global augment
 
 declare global {
 	interface Window {
 		env: typeof envType;
+		Application: new (directory: string, args: any[]) => executables.Application;
+		BackgroundProcess: new (directory: string, args: any[]) => executables.BackgroundProcess;
 	}
 
 	const env: typeof envType;
+	const Application: new (directory: string) => executables.Application;
+	const BackgroundProcess: new (directory: string) => executables.BackgroundProcess;
 }
