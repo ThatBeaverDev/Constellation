cbc57d1930c61166874a5e48b0f681c43b216202
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-07-02 20:48:12 +0100

    Create applications.ts

---
+++ src/tests/applications.ts
@@ -0,0 +1,10 @@
+import { TestingError } from "./errors";
+import * as windows from "../windows/windows.js";
+
+const tests = {
+	newApp: async () => {}
+};
+
+export default tests;
+
+export const testNames = Object.keys(tests);
