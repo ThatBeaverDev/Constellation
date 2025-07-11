a423fed04e1e22a0dbdfb0da26890a855614ab8f
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-07-11 19:50:36 +0100

    Update blobifier to allow the directory of BLOBs to be retraced

--- src/lib/blobify.ts
+++ src/lib/blobify.ts
@@ -1,5 +1,7 @@
 import fs from "../fs.js";
 
+export const index: any = {};
+
 export function blobify(text: string, mime = "text/plain") {
 	const blob = new Blob([text], {
 		type: mime
@@ -7,6 +9,32 @@ export function blobify(text: string, mime = "text/plain") {
 	const location = URL.createObjectURL(blob);
 
 	return location;
+}
+
+export async function blobifyDirectory(location: string, mime = "text/plain") {
+	const read = await env.fs.readFile(location);
+	if (!read.ok) throw read.data;
+
+	const text = read.data;
+
+	const blob = new Blob([text], {
+		type: mime
+	});
+	const URI = URL.createObjectURL(blob);
+
+	index[URI] = location;
+
+	return URI;
+}
+
+export function translateAllBlobURIsToDirectories(text: string): string {
+	let str = text;
+
+	for (const i in index) {
+		str = str.replaceAll(i, index[i]);
+	}
+
+	return str;
 }
 
 export async function readAndBlobify(directory: string, mime = "text/plain") {
