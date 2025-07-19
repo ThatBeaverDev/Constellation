f0bb0c37371e724084e4d65a36fd1cc0e161a956
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-07-19 11:34:20 +0100

    Create panic, a way for Constellation to show a graphical error onscreen

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -1,3 +1,5 @@
+import panic from "./panic.js";
+
 // https://stackoverflow.com/questions/9267899/how-can-i-convert-an-arraybuffer-to-a-base64-encoded-string
 function _arrayBufferToBase64(buffer: ArrayBuffer) {
 	let binary = "";
@@ -7,6 +9,14 @@ function _arrayBufferToBase64(buffer: ArrayBuffer) {
 		binary += String.fromCharCode(bytes[i]);
 	}
 	return binary;
+}
+
+if (window.crypto.subtle == undefined) {
+	panic(
+		new Error(
+			"crypto.subtle is not present - Constellation's cryptographical security systems cannot start."
+		)
+	);
 }
 
 export async function sha512(text: string) {
