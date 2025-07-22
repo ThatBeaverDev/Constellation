69a35c33c859651e663249c3e8f8a114f598199c
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-07-22 07:22:42 +0100

    Create tcpkg and tcupkg

---
+++ src/apps/code/finder/lib/tcupkg.ts
@@ -0,0 +1,67 @@
+import TerminalAlias from "../../../../lib/terminalAlias";
+
+export default async function tcupkg(
+	parent: TerminalAlias,
+	target: string,
+	output: string
+) {
+	const targetRel = parent.env.fs.resolve(parent.path, target);
+	const directory = parent.env.fs.resolve(parent.path, output);
+
+	let content = await env.fs.readFile(targetRel);
+	if (!content.ok) throw content.data;
+	content = content.data;
+
+	let json;
+
+	await env.fs.createDirectory(directory);
+
+	try {
+		json = JSON.parse(content);
+	} catch {
+		throw new Error(`Package is not packaged properly.`);
+	}
+
+	for (const path of json.directories) {
+		const relative = env.fs.resolve(directory, path);
+
+		await env.fs.createDirectory(relative);
+	}
+
+	const awaitFiles = [];
+	for (const path in json.files) {
+		const data = json.files[path];
+		const relative = env.fs.resolve(directory, path);
+
+		const type = data.type == undefined ? "string" : data.type;
+
+		switch (type) {
+			case "string":
+				awaitFiles.push(env.fs.writeFile(relative, data));
+				break;
+			case "binary":
+				awaitFiles.push(env.fs.writeFile(relative, data.data));
+				break;
+			default:
+				throw new Error(
+					"Unknown key type within files object: '" + type + "'"
+				);
+		}
+	}
+
+	for (const item of awaitFiles) {
+		await item;
+	}
+
+	if (json.files["postunpkg.js"] !== undefined) {
+		const incl = await env.include(
+			env.fs.resolve(directory, "postunpkg.js")
+		);
+
+		const fnc = incl.default;
+
+		if (typeof fnc == "function") {
+			fnc(directory);
+		}
+	}
+}
