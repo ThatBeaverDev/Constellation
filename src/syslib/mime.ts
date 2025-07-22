823c3d82343df21104b508ce95b9b6e8abd78a65
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-07-22 07:22:31 +0100

    Create mime library for converting file extensions to MIME types

---
+++ src/syslib/mime.ts
@@ -0,0 +1,19 @@
+const mimeData = await env.include("/System/CoreLibraries/mimeFiles/mimes.js");
+const mimes = mimeData.default;
+
+if (mimes == undefined) {
+	throw new Error(
+		"Mimes file is empty and therefore mime types cannot be processed."
+	);
+}
+
+export function getType(extension: string): string | null {
+	for (const type in mimes) {
+		const extensions = mimes[type];
+		if (extensions.includes(extension)) {
+			return type;
+		}
+	}
+
+	return null;
+}
