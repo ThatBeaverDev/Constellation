7507b2c61983953fa524792475405978277403b8
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-07-26 12:57:39 +0100

    Update appsImportReplacements.ts

--- src/apps/appsImportReplacements.ts
+++ src/apps/appsImportReplacements.ts
@@ -49,7 +49,6 @@ export async function rewriteImportsAsync(
 				currentPath.textBeforeLast("/"),
 				specifier
 			);
-			console.log(currentPath, specifier, resolved);
 			// blobify it
 			const blobUrl = await blobifyModule(resolved);
 			return {
