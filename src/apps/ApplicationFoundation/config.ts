6488f58293224ea2e492534ea00b022cbf68bd0b
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-07-29 10:22:54 +0100

    Remove ApplicationFoundation

--- src/apps/ApplicationFoundation/config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-	name: "ApplicationFoundation Demo App",
-	description: "An Example app from which all others are forked..",
-	category: "Productivity",
-	author: "Constellation",
-	version: 1
-};
