d5409f739d3bce1f41bbe4b67857803b544e8936
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-08-02 21:38:32 +0100

    Update dock to group windows of the same application under one icon with a context menu onclick

--- src/apps/dock/resources/dock.ts
+++ src/apps/dock/resources/dock.ts
@@ -2,23 +2,43 @@ import { Renderer } from "../../../lib/uiKit/uiKit";
 import { ApplicationAuthorisationAPI } from "../../../security/env";
 import { WindowAlias } from "../../../security/definitions";
 import dockAndDesktop from "../tcpsys/app";
+// @ts-expect-error
+import { getAppConfig } from "/System/CoreLibraries/pathinf.js";
+
+export interface dockConfig {
+	pins: string[];
+}
+interface Program {
+	windows: WindowAlias[];
+	isPinned: boolean;
+	manifest: ApplicationManifest;
+}
 
 export default class dock {
-	parent: dockAndDesktop;
-	renderer: Renderer;
-	env: ApplicationAuthorisationAPI;
+	readonly parent: dockAndDesktop;
+	readonly renderer: Renderer;
+	readonly env: ApplicationAuthorisationAPI;
 	winAPI?: typeof import("../../../windows/windows");
 
 	dockHeight: number = 50;
 	dockPadding: number = 10;
 	wins: WindowAlias[] = [];
+	programs: Record<string, Program> = {};
 	ok: boolean = false;
 	width: number = 0;
+	config: dockConfig;
+	pinsInfo?: {
+		directory: string;
+		manifest: ApplicationManifest;
+	}[];
+	tick: number = 0;
 
 	constructor(parent: dockAndDesktop) {
 		this.parent = parent;
 		this.renderer = parent.renderer;
 		this.env = parent.env;
+
+		this.config = parent.config.dock;
 
 		this.init();
 	}
@@ -33,35 +53,83 @@ export default class dock {
 
 		this.winAPI = await this.parent.env.include("/System/windows.js");
 
-		this.refresh();
+		await this.refresh();
 
 		this.ok = true;
 	}
 
-	refresh() {
+	async refresh() {
 		this.wins = this.parent.env.windows.all();
+
+		this.pinsInfo = [];
+
+		for (const i in this.config.pins) {
+			const pin = this.config.pins[i];
+
+			this.pinsInfo[i] = {
+				directory: pin,
+				manifest: await getAppConfig(pin)
+			};
+		}
+
+		const programs: Record<string, Program> = {};
+
+		programs[this.parent.directory] = {
+			windows: [],
+			isPinned: true,
+			manifest: await getAppConfig(this.parent.directory)
+		};
+
+		if (this.pinsInfo !== undefined) {
+			for (const prog of this.pinsInfo) {
+				const progDir = prog.directory;
+
+				if (programs[progDir] == undefined) {
+					programs[progDir] = {
+						windows: [],
+						isPinned: true,
+						manifest: await getAppConfig(progDir)
+					};
+				}
+			}
+		}
+
+		for (const win of this.wins) {
+			const winDir = win.applicationDirectory;
+			if (winDir == undefined) continue;
+
+			if (programs[winDir] == undefined) {
+				programs[winDir] = {
+					windows: [],
+					isPinned: this.config.pins.includes(winDir),
+					manifest: await getAppConfig(winDir)
+				};
+			}
+
+			programs[winDir].windows.push(win);
+		}
+
+		this.programs = programs;
+
+		this.tick = 0;
 	}
 
-	calculateDockWidth(scale: number = 1) {
-		const iconWidth = this.dockHeight - this.dockPadding * 2;
-		const iconGap = iconWidth + this.dockPadding;
+	render() {
+		if (++this.tick == 5) {
+			this.tick = -100000; // refresh sets it to zero.
 
-		return iconGap * this.wins.length + this.dockPadding;
-	}
+			this.refresh();
+		}
 
-	async render() {
 		if (this.ok !== true) return;
 		if (this.winAPI == undefined) return;
-
-		this.refresh();
-
-		const wins = this.wins;
 
 		const iconWidth = this.dockHeight - this.dockPadding * 2;
 		const iconScale = iconWidth / 24;
 
 		const iconGap = iconWidth + this.dockPadding;
-		const targetWidth = this.calculateDockWidth();
+		const targetWidth = iconGap * Object.keys(this.programs).length + this.dockPadding;
+
 		this.width = Math.round(this.width + (targetWidth - this.width) / 5);
 
 		let scale = 1;
@@ -69,14 +137,13 @@ export default class dock {
 			scale = this.renderer.windowWidth / this.width;
 		}
 
+		// calculate positioning
 		const dockLeft = (this.renderer.windowWidth - this.width * scale) / 2;
-
 		const dockPadding = this.dockPadding * scale;
-
 		let x = dockLeft + this.dockPadding * scale;
-		let y =
-			this.renderer.windowHeight - this.dockHeight * scale + dockPadding;
+		let y = this.renderer.windowHeight - this.dockHeight * scale + dockPadding;
 
+		// render back
 		this.renderer.box(
 			dockLeft,
 			this.renderer.windowHeight - this.dockHeight * scale,
@@ -88,56 +155,105 @@ export default class dock {
 			}
 		);
 
-		for (const win of wins) {
-			const iconID = this.renderer.icon(
-				x,
-				y,
-				win.iconName,
-				iconScale * scale
-			);
+		const drawIcon = (directory: string, program: Program) => {
+			const name = program.manifest.name;
+			const icon = program.manifest.icon;
+
+			const iconID = this.renderer.icon(x, y, program.manifest.icon, iconScale * scale);
+
+			if (program.windows.length !== 0) {
+				// the app is running.
+
+				this.renderer.box(x + 0.5 * iconWidth - 2.5, y + iconWidth + 3, 5, 5, {
+					borderRadius: 100,
+					background: "white"
+				});
+			}
+
+			let win: WindowAlias | undefined;
+			if (program.windows.length == 1) {
+				win = program.windows[0];
+			}
 
 			this.renderer.onClick(
 				iconID,
-				() => {
+				(left: number, top: number) => {
 					if (this.winAPI == undefined) return;
-					if (win == undefined) return;
 
-					if (win.minimised) {
-						win.unminimise();
+					if (win == undefined) {
+						// check whether the app is even running at all
+						if (program.windows.length == 0) {
+							// start the app.
+							this.env.exec(directory);
+						} else {
+							// show preview box of all the windows
+							let contextMenuItems: Record<string, Function> = {};
+
+							for (const i in program.windows) {
+								const win = program.windows[i];
+
+								contextMenuItems[`${win.iconName}-:-${win.name};${i}`] = () => {
+									if (this.winAPI == undefined) return;
+
+									if (win.minimised) {
+										win.unminimise();
+									} else {
+										win.minimise();
+									}
+
+									this.winAPI.focusWindow(win.winID);
+								};
+							}
+
+							this.renderer.setContextMenu(left, top, `Windows of ${name}`, contextMenuItems);
+						}
 					} else {
-						win.minimise();
-					}
+						// just focus the one window we have.
+						if (win.minimised) {
+							win.unminimise();
+						} else {
+							win.minimise();
+						}
 
-					this.winAPI.focusWindow(win.winID);
+						this.winAPI.focusWindow(win.winID);
+					}
 				},
 				(left: number, top: number) => {
 					// menu items
-					const contextMenuItems = {
-						"Show in Finder": () =>
-							this.env.exec("/Applications/Finder.appl", [
-								env.fs.resolve(win.applicationDirectory, "..")
-							]),
-						Minimise: () => win.minimise(),
-						Restore: () => win.unminimise(),
-						Close: () => win.close()
+					let contextMenuItems: Record<string, Function> = {
+						"folder-open-:-Show in Finder": () =>
+							this.env.exec("/Applications/Finder.appl", [env.fs.resolve(directory, "..")])
+					};
+
+					if (win !== undefined) {
+						contextMenuItems = {
+							...contextMenuItems,
+							"minimize-2-:-Minimise": () => win.minimise(),
+							"expand-:-Restore": () => win.unminimise(),
+							"x-:-Close": () => win.close()
+						};
+					} else {
+						contextMenuItems = {
+							...contextMenuItems,
+							"app-window-mac-:-Open": () => env.exec(directory)
+						};
+					}
+
+					contextMenuItems = {
+						...contextMenuItems,
+						"dock-:-Unpin From Dock": () => {}
 					};
 
 					// show menu
-					this.renderer.setContextMenu(
-						left,
-						top,
-						win.name,
-						contextMenuItems
-					);
-
-					// remove menu on click
-					//this.renderer.awaitClick(() => {
-					//	this.renderer.removeContextMenu();
-					//});
+					this.renderer.setContextMenu(left, top, name, contextMenuItems);
 				}
 			);
 
 			x += iconGap * scale;
+		};
+
+		for (const directory in this.programs) {
+			drawIcon(directory, this.programs[directory]);
 		}
 	}
 }
--- src/apps/dock/tcpsys/app.ts
+++ src/apps/dock/tcpsys/app.ts
@@ -1,11 +1,28 @@
 import { IPCMessage } from "../../messages.js";
-import dock from "../resources/dock.js";
+import dock, { dockConfig } from "../resources/dock.js";
 import menubar from "../resources/menubar.js";
+
+interface menubarConfig {}
 
 export default class dockAndDesktop extends Application {
 	dock?: dock;
 	menubar?: menubar;
 	showApps: boolean = false;
+	config: {
+		dock: dockConfig;
+		menubar: menubarConfig;
+		name: string;
+		icon: string;
+	} = {
+		dock: {
+			pins: ["/Applications/Finder.appl", "/Applications/Settings.appl", "/Applications/Search.appl"]
+		},
+		menubar: {},
+		name: "Constellation",
+		icon: "/System/CoreAssets/Logos/Constellation-lucide.svg"
+	};
+	oldConfig: string = "";
+	tick: number = 0;
 
 	async init() {
 		this.renderer.makeWindowInvisible();
@@ -13,13 +30,13 @@ export default class dockAndDesktop extends Application {
 		this.renderer.hideWindowCorners();
 
 		//this.renderer.setIcon("dock")
-		this.renderer.setIcon(
-			"/System/CoreAssets/Logos/Constellation-White.svg"
-		);
-		this.renderer.windowName = "Constellation";
+		this.renderer.setIcon(this.config.icon);
+		this.renderer.windowName = this.config.name;
 
 		this.registerKeyboardShortcut("Search", "KeyZ", ["AltLeft"]);
 		this.registerKeyboardShortcut("Library", "KeyX", ["AltLeft"]);
+
+		await this.loadConfig();
 
 		this.dock = new dock(this);
 		this.menubar = new menubar(this);
@@ -29,10 +46,42 @@ export default class dockAndDesktop extends Application {
 		};
 	}
 
+	async loadConfig() {
+		const list = await this.env.fs.listDirectory(env.fs.resolve(this.directory, "./data"));
+		if (!list.ok) throw list.data;
+		const configs = list.data;
+
+		if (configs.includes(this.env.userID + ".json")) {
+			// we have the config, let's load it.
+			const read = await this.env.fs.readFile(
+				env.fs.resolve(this.directory, "./data/" + this.env.userID + ".json")
+			);
+			if (!read.ok) throw read.data;
+			const config = JSON.parse(read.data);
+
+			this.config = config;
+		} else {
+			// no config, just leave the default in place.
+		}
+	}
+
+	async commitConfig() {
+		const configString = JSON.stringify(this.config);
+
+		const configDirectory = env.fs.resolve(this.directory, "./data/" + this.env.userID + ".json");
+		await env.fs.writeFile(configDirectory, configString);
+	}
+
 	frame() {
 		// resize and reposition
 		this.renderer.moveWindow(0, 0);
 		this.renderer.resizeWindow(window.innerWidth, window.innerHeight);
+
+		// save config if it's changed.
+		if (this.oldConfig !== JSON.stringify(this.config)) {
+			this.oldConfig = JSON.stringify(this.config);
+			this.commitConfig();
+		}
 
 		this.renderer.clear();
 
