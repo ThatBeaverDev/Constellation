19edbab6ffd7e6e2f532a3847397eafbc5c4c4ab
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-09-07 18:11:45 +0100

    Implement `tcpkg` and `tcupkg` into the kernel

---
+++ src/lib/packaging/definitions.ts
@@ -0,0 +1,21 @@
+import mimes from "../../syslib/mime/mimes.js"
+import { tcpkg } from "./tcpkg.js";
+import { tcupkg } from "./tcupkg.js";
+
+export interface ConstellationFileIndex {
+	directories: string[];
+	files: Record<string, string | { type: "string" | "binary"; data: string }>;
+}
+
+export function getMimeType(extension: string): string | null {
+	for (const type in mimes) {
+		const extensions = mimes[type];
+		if (extensions.includes(extension)) {
+			return type;
+		}
+	}
+
+	return null;
+}
+
+export { tcpkg, tcupkg }
