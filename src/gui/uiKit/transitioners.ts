369bb05e55f125f6345c0cca2672aac2514e26f8
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-09-08 20:56:38 +0100

    import should annotate with '.js'

--- src/gui/uiKit/transitioners.ts
+++ src/gui/uiKit/transitioners.ts
@@ -1,4 +1,4 @@
-import { step } from "./definitions";
+import { step } from "./definitions.js";
 
 export default class uiKitTransitioners {
 	textboxElem: HTMLInputElement | HTMLTextAreaElement | undefined;
