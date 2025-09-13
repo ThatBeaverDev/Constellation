c9535b53a3d1ccffa582f344cbb919aa9906b67a
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-09-13 07:44:36 +0100

    UserPrompt has a forced zIndex

--- src/gui/windows/windows.ts
+++ src/gui/windows/windows.ts
@@ -905,6 +905,8 @@ export class UserPrompt extends GraphicalWindowClass {
 		this.container.style.background =
 			"rgb(from var(--backgroundColour) r g b / 50%)";
 		this.container.style.backdropFilter = "blur(10px)";
+		this.container.style.zIndex = "10000";
+		this.forcedZIndex = 100000;
 
 		this.hideHeader();
 
