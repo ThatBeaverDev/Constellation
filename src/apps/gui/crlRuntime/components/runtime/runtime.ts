dd37435c0ba8d45a07897ce6cd3f6b72763d6f90
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-10-04 11:38:31 +0100

    Implement better scoping for blocks and fix reassigning variables

--- src/apps/gui/crlRuntime/components/runtime/runtime.ts
+++ src/apps/gui/crlRuntime/components/runtime/runtime.ts
@@ -61,12 +61,76 @@ export class CrlRuntime {
 			}
 		}
 
+		this.debug(
+			"Variable by name " + name + " is not declared within scopes,",
+			scopes
+		);
+		throw new Error("Variable by name " + name + " is not declared.");
+	}
+	reassignVariableInScopes(
+		scopes: RuntimeScope[],
+		name: string,
+		value: RuntimeValue
+	) {
+		for (const scope of scopes) {
+			let val = scope.variables.get(name);
+
+			if (val == undefined) {
+				this.debug("Scope", scope.variables, `doesn't have ${name}`);
+			} else {
+				this.debug(
+					"Scope",
+					scope.variables,
+					`has ${name}. reassigning.`
+				);
+
+				val.value = value;
+				scope.variables.set(name, val);
+
+				return;
+			}
+		}
+
+		this.debug(
+			"Variable by name " + name + " is not declared within scopes,",
+			scopes
+		);
 		throw new Error("Variable by name " + name + " is not declared.");
 	}
 
-	async evalBlock(scopes: RuntimeScope[], block: AstNode[]) {
+	async evalBlock(
+		scopes: RuntimeScope[],
+		block: AstNode[],
+		variables?: Map<string, RuntimeValue>
+	) {
+		let extendedScopes = [...scopes];
+
+		// function parameter scope
+		if (variables) {
+			// create scope
+			const variableScope = new Scope(this, this.isDebug);
+			extendedScopes = [...extendedScopes, variableScope];
+
+			// assign variables
+			for (const variableName in variables) {
+				const variableValue = variables.get(variableName);
+				if (!variableValue)
+					throw new Error(
+						"Arguement passed in function call went missing."
+					);
+
+				variableScope.variables.set(variableName, {
+					type: "constant",
+					value: variableValue
+				});
+			}
+		}
+
+		// new variables scope
 		const extendingScope = new Scope(this, this.isDebug);
-		const extendedScopes = [...scopes, extendingScope];
+		extendedScopes = [...extendedScopes, extendingScope];
+
+		this.debug("Running block with scopes", extendedScopes);
 
 		for (const node of block) {
 			await this.evalNode(extendedScopes, node);
@@ -389,14 +453,11 @@ export class CrlRuntime {
 			}
 
 			case "reassignment": {
-				const target = holdingScope.variables.get(data.name);
-				if (target == undefined)
-					throw new Error(`Variable ${data.name} is not defined!`);
-
+				// eval the node
 				const value = await this.evalNode(scopes, data.value);
-				target.value = value;
 
-				holdingScope.variables.set(data.name, target);
+				// assign
+				this.reassignVariableInScopes(scopes, data.name, value);
 				break;
 			}
 
