c34e7d30d98e0cbc4319f75926a5736bc2531685
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-11-21 17:11:23 +0000

    Allow software update to be ran by users without operator permission

--- src/apps/services/SoftwareUpdateHandler/bin/service.ts
+++ src/apps/services/SoftwareUpdateHandler/bin/service.ts
@@ -1,13 +1,34 @@
-import ConstellationKernel from "../../../../system/kernel";
-
-export default class SoftwareUpdateInstaller extends Service {
-	#ConstellationKernel?: ConstellationKernel;
-
+export default class SoftwareUpdateInstaller extends GuiApplication {
 	async init() {
-		this.#ConstellationKernel = this.env.getKernel();
+		// hide
+		this.renderer.moveWindow(-1000, -1000);
+		this.renderer.resizeWindow(0, 0);
 
-		const kernel = this.#ConstellationKernel;
+		this.renderer.hideWindowHeader();
 
-		kernel.triggerUpdate();
+		// update the system
+		const update = () => {
+			const kernel = this.env.getKernel();
+
+			kernel.triggerUpdate();
+		};
+
+		try {
+			update();
+		} catch (e: unknown) {
+			if (((e as Error).name = "PermissionsError")) {
+				const adminPassword = await this.renderer.askUserQuestion(
+					"Please enter admin password to update Constellation:",
+					"The administrator password is required to update the system.",
+					"rotate-ccw"
+				);
+
+				await this.env.users.switch("admin", adminPassword);
+
+				update();
+			} else {
+				throw e;
+			}
+		}
 	}
 }
