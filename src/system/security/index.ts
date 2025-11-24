220053a5823ca9b8afd6a039efd7e60f9c02f06f
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-11-24 19:38:41 +0000

    Adjustments to types

--- src/system/security/index.ts
+++ src/system/security/index.ts
@@ -1,18 +1,21 @@
 import ConstellationKernel, { Terminatable } from "..//kernel.js";
 import { EnvironmentCreator } from "./env.js";
-import { Permissions } from "./permissions.js";
+import { ConstellationPermissionsManager } from "./permissions.js";
 import Users from "./users.js";
 
 export default class Security {
 	env: EnvironmentCreator & Terminatable;
-	permissions: Permissions & Terminatable;
+	permissions: ConstellationPermissionsManager & Terminatable;
 	users: Users & Terminatable;
 
 	constructor(ConstellationKernel: ConstellationKernel) {
 		const filesystem = ConstellationKernel.fs;
 
 		this.users = new Users(ConstellationKernel);
-		this.permissions = new Permissions(filesystem, ConstellationKernel);
+		this.permissions = new ConstellationPermissionsManager(
+			filesystem,
+			ConstellationKernel
+		);
 		this.env = new EnvironmentCreator(
 			filesystem,
 			this.users,
