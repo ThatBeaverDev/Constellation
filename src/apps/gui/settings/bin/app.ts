680cad4464b4617541a63f8c2a6e6746e69cb167
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-11-24 19:55:29 +0000

    Refactor System Settings to use PanelKit

--- src/apps/gui/settings/bin/app.ts
+++ src/apps/gui/settings/bin/app.ts
@@ -1,19 +1,19 @@
-import { SettingsBody } from "../components/body.js";
+import PanelKit from "panelkit";
 import { SettingsPages } from "../components/pages.js";
-import { SettingsSidebar } from "../components/sidebar.js";
 
 export default class systemSettings extends GuiApplication {
-	sidebar?: SettingsSidebar;
-
-	body?: SettingsBody;
 	page: keyof SettingsPages = "Home";
+	pages!: SettingsPages;
+	panelkit: PanelKit = new PanelKit(this.renderer);
 
 	async init() {
-		// body
-		this.body = new SettingsBody(this);
+		this.panelkit.sidebarWidth = 110;
 
-		// sidebar
-		this.sidebar = new SettingsSidebar(this, this.body, 150);
+		// pages
+		this.pages = new SettingsPages(
+			this,
+			(page: keyof SettingsPages) => (this.page = page)
+		);
 
 		this.renderer.windowName = "System Settings";
 		this.renderer.setIcon("cog");
@@ -22,8 +22,75 @@ export default class systemSettings extends GuiApplication {
 	async frame() {
 		this.renderer.clear();
 
-		if (this.sidebar !== undefined) this.sidebar.render();
-		if (this.body !== undefined) await this.body.render();
+		// sidebar
+		this.panelkit.sidebar(
+			{ type: "title", text: "Settings" },
+			{
+				type: "item",
+				text: "Home",
+				icon: "house",
+				callback: () => {
+					this.page = "Home";
+				}
+			},
+
+			{ type: "title", text: "Networking" },
+			{
+				type: "item",
+				text: "Updates",
+				icon: "cloud-download",
+				callback: () => {
+					this.page = "Updates";
+				}
+			},
+			{
+				type: "item",
+				text: "Network",
+				icon: "globe",
+				callback: () => {
+					this.page = "Network";
+				}
+			},
+			{
+				type: "item",
+				text: "Cloud",
+				icon: "cloud",
+				callback: () => {
+					this.page = "Cloud";
+				}
+			},
+
+			//{ type: "title", text: "Graphical Shell" },
+			//{
+			//	type: "item",
+			//	text: "Dock",
+			//	icon: "dock",
+			//	callback: () => {
+			//		this.page = "Dock";
+			//	}
+			//},
+			//{
+			//	type: "item",
+			//	text: "Menubar",
+			//	icon: "panels-top-left",
+			//	callback: () => {
+			//		this.page = "Menubar";
+			//	}
+			//},
+
+			{ type: "title", text: "System" },
+			{
+				type: "item",
+				text: "Users",
+				icon: "users",
+				callback: () => {
+					this.page = "Users";
+				}
+			}
+		);
+
+		// body
+		await this.pages[this.page].bind(this.pages)();
 
 		this.renderer.commit();
 	}
