01e64b05058e578a92ed21a238e3b88a2d3d86db
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2025-12-02 17:53:17 +0000

    Create `open` command

--- src/apps/gui/dock/lib/open.ts
+++ src/apps/gui/dock/lib/open.ts
@@ -1,1 +1,8 @@
 // TODO: Finish me!
+
+import TerminalAlias from "../../../../system/lib/terminalAlias";
+import { openFile } from "gui";
+
+export default function open(parent: TerminalAlias, file: string) {
+	openFile(parent.env, parent.env.fs.resolve(parent.path, file));
+}
